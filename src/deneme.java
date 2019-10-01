db9b54251d17ed6bf0cea09a896d670d2ff66bc7
nkeremcoskun <krmcskn@users.noreply.github.com>
2019-10-01 13:16:57 +0300

    Update deneme.java
    NABIONUZ

--- src/deneme.java
+++ src/deneme.java
@@ -4,5 +4,6 @@ public static void main(String[] args) {
 	System.out.println("Hello world ilyas");
 	System.out.println("hello world beng");
 	System.out.println("alperlerpsl");
+	System.out.println("SELAMIN ALEYKÜM DÜNYA");
 }
 }
