8e44e36928ff4dccdc6409f182f99d39bb8eb60f
alpersayin <alpersayin@users.noreply.github.com>
2019-10-01 10:55:25 +0300

    Update deneme.java
    Alper düzenlediiiiiii

--- src/deneme.java
+++ src/deneme.java
@@ -3,5 +3,6 @@ public class deneme {
 public static void main(String[] args) {
 	System.out.println("Hello world ilyas");
 	System.out.println("hello world beng");
+	System.out.println("alperlerpsl");
 }
 }
