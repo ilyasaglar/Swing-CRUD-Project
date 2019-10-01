70ec5490fb4c5d14ceeebc769b4bf76a42975bbe
ilyas ağlar <ilyas_693@hotmail.com>
2019-10-01 16:31:34 +0300

    Employees
    Employee Class'lar

---
+++ src/Employee.java
@@ -0,0 +1,90 @@
+
+public class Employee {
+	int employee_id;
+	String first_name;
+	String last_name;
+	String email;
+	int phone_number;
+	String hire_date;
+	int job_id;
+	int salary;
+	int manager_id;
+	int department_id;
+	
+	
+	
+	public Employee(int employee_id, String first_name, String last_name, String email, int phone_number,
+			String hire_date, int job_id, int salary, int manager_id, int department_id) {
+		super();
+		this.employee_id = employee_id;
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.email = email;
+		this.phone_number = phone_number;
+		this.hire_date = hire_date;
+		this.job_id = job_id;
+		this.salary = salary;
+		this.manager_id = manager_id;
+		this.department_id = department_id;
+	}
+
+	public Employee() {
+		super();
+	}
+
+	public int getEmployee_id() {
+		return employee_id;
+	}
+	public void setEmployee_id(int employee_id) {
+		this.employee_id = employee_id;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getPhone_number() {
+		return phone_number;
+	}
+	public void setPhone_number(int phone_number) {
+		this.phone_number = phone_number;
+	}
+	public int getJob_id() {
+		return job_id;
+	}
+	public void setJob_id(int job_id) {
+		this.job_id = job_id;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	public int getManager_id() {
+		return manager_id;
+	}
+	public void setManager_id(int manager_id) {
+		this.manager_id = manager_id;
+	}
+	public int getDepartment_id() {
+		return department_id;
+	}
+	public void setDepartment_id(int department_id) {
+		this.department_id = department_id;
+	}
+		
+}
