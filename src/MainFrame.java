98d0635b08cbd35cbcc6b7867b8e9928348ce30c
alpersayin <alpersayin@users.noreply.github.com>
2019-10-07 01:27:51 +0300

    Countries güncellendi
    Region ID için cbox eklendi.

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,7 +1,4 @@
 import javax.swing.JFrame;
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 
 import java.awt.event.ActionListener;
